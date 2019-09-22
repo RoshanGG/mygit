758c86541b54bd944da4654f64cd47782edce962
johnson <dajiaozhi_china@163.com>
2019-09-22 10:02:47 +0800

    creat a hello file

---
+++ hellogit.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hellogit.c
+	> Author: johnson
+	> Mail: dajiaozhi_china@163.com 
+	> Created Time: 2019年09月22日 星期日 09时58分50秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+	printf("Hello git\r\n");
+	return 0;
+}
+
+
